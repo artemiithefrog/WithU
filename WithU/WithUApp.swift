f25e2ba212f73c09a649d650597427ee26614b4a
Artemii Malyshev <115695294+artemiithefrog@users.noreply.github.com>
2024-05-28 22:04:39 +0200

    Initial Commit

---
+++ WithU/WithUApp.swift
@@ -0,0 +1,17 @@
+//
+//  WithUApp.swift
+//  WithU
+//
+//  Created by artemiithefrog . on 28.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct WithUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
